308fe9fb365413c251085c1d71db4f3110ba52d2
Abhishek Thakur <63930716+Abhishek42u@users.noreply.github.com>
2022-01-05 01:08:12 +0530

    Create Find Missing And Repeating .cpp

---
+++ Find Missing And Repeating .cpp
@@ -0,0 +1,29 @@
+ int *ans = new int[2];
+      unordered_map<int,int> mp;
+      
+      int maxi = INT_MIN;
+      for(int i=0; i<n; i++)
+      {
+          maxi = max(maxi,arr[i]);
+          mp[arr[i]]++;
+      }
+      
+      
+      for(int i=0; i<n; i++)
+      {
+          if(mp[arr[i]] == 2)
+          {
+              ans[0] = arr[i];
+              break;
+          }
+      }
+      
+      for(int i=1; i<=maxi+1; i++)
+      {
+          if(mp.count(i) == false)
+          {
+              ans[1] = i;
+              break;
+          }
+      }
+      return ans;
